2f9c156033f32e5a555b971ca056314a39e569fe
Neff10 <st.tunkara@gmail.com>
2019-05-06 12:53:39 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a>
+        Accessibility Life
+      </a>
     </div>
   );
 }
