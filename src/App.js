1ce90a7b41617f77ac3d2d2f4ab5db396fe2c9e1
Neff10 <st.tunkara@gmail.com>
2019-05-09 13:48:42 +0100

    test2

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ import './App.css';
 function App() { 
   return (
     <div className="App">
+
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
         <a class="twitter-timeline" data-width="1000" data-height="400" href="https://twitter.com/TfLAccess?ref_src=twsrc%5Etfw">Tweets by TfLAccess</a>
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
