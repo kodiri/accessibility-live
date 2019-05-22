32c16810bf021b0b6a2bd2265e6ea46a756284fb
ironeagle245 <carl.cross@yahoo.co.uk>
2019-05-22 18:56:02 +0100

    Added twitter embed to details

--- src/Frontend/component/Details/Details.jsx
+++ src/Frontend/component/Details/Details.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import './Details.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import DetailsChart from './Images/Details-Chart.png';
 
 export default function Details() {
     return (
-        <Router>
-            <div className="app">
-                <h1 className="section-heading">
-                    Details
-                </h1>
-                <div className="container">
-                    <a className="twitter-timeline"
-                        data-width="700"
-                        data-height="700"
-                        href="https://twitter.com/TfLAccess?ref_src=twsrc%5Etfw"
-                    > Tweets by TfLAccess
-                    </a>
-                    <div className="details">
-                        <p1 className="p1-heading">Some details about diability.</p1>
-                        <p2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p2>
-                    </div>
-                </div>
-            </div>
-        </Router>
+        <div className="container">
+            <h1>
+                Details
+            </h1>
+            <a class="twitter-timeline"
+                    data-width="600"
+                    data-height="600"
+                    href="https://twitter.com/Access_Live_?ref_src=twsrc%5Etfw"
+                    >Tweets by Access_Live_
+            </a> 
+            <img src={DetailsChart} />
+        </div>
     );
 }
