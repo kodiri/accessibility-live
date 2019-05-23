c7f48eb8e4a401ba8972e7a567df1874acdbf4f9
S-B1 <sharbraham@icloud.com>
2019-05-23 17:50:08 +0100

    Connect About and Meet the Team.

--- src/Frontend/routes/AboutPage.jsx
+++ src/Frontend/routes/AboutPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import About from '../component/About/AboutUs';
+import MeetTheTeam from './HomePage/MeetTheTeam/MeetTheTeam'; 
+
 
 export default function AboutPage() {
   return (
     <div className="AboutPage">
       <p>About Page</p>
+      <About/>
+      <MeetTheTeam/>
     </div>
   );
 }
