6965c9cbd64dd1f78386158821c1b3b26e8be1c8
Sule Tunkara <st.tunkara@gmail.com>
2019-05-27 17:35:25 +0100

    Finished Adding Cards and Worked on their Responsiveness (#69)
    * Please accept these changes
    
    * Working on the carousel
    
    * How page should look now
    
    * Finished the gallery Component
    
    * Adding new cards
    
    * Another one
    
    * added a test card
    
    * Added All Cards
    
    * Cards and their responsiveness

--- src/Frontend/component/Gallery/Cards/Cards.jsx
+++ src/Frontend/component/Gallery/Cards/Cards.jsx
@@ -11,13 +11,13 @@ const Card = ({ service }) => {
   const { index, picture, title, description } = service;
   return (
     <div id={`card-${index}`} className="cardCss" style={divStyle1}>
-      <img src={picture} alt={title} alt="Image" className="cardPic" />
-      <div className="tags">
-        <div className="travelTag"><p>Door-to-Door</p></div>
-        <div className="title"><h1>{title}</h1></div>
-        <div className="description"><p>{description}</p></div>
-      </div>
-      <a href="#0" className="readmeTag">Read More...</a>
+        <img src={picture} alt={title} alt="Image" className="cardPic" />
+        <div className="tags">
+          <div className="travelTag"><p>Door to Door</p></div>
+          <div className="title"><h1>{title}</h1></div>
+          <div className="description"><p>{description}</p></div>
+        </div>
+        <a href="#0" className="readmeTag">Read More</a>
     </div>
   );
 }
