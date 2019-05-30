6f4e46bddd0c5c1811e437f03099eb4a5e7801c3
Neff10 <st.tunkara@gmail.com>
2019-05-30 02:16:04 +0100

    Sorted conflicts

--- src/Frontend/component/Details/Details.jsx
+++ src/Frontend/component/Details/Details.jsx
@@ -5,15 +5,28 @@ import DetailsChart from './Images/Details-Chart.png';
 
 export default function Details() {
     return (
-        <div className="container">
-            <div className="container2">
-                <h1>
-                    Details
-                </h1>
-                <div className="twitter-timeline">
-                <a class="twitter-timeline" data-width="700" data-height="700" data-theme="light" href="https://twitter.com/Access_Live_?ref_src=twsrc%5Etfw">Tweets by Access_Live_</a>
+        <div className="Details">
+            <h1 className="Details__title">
+                Details
+            </h1>
+            <div className="Details__flex_container">
+                <div
+                    className="Details__flex_container--item-1">
+                    <a
+                        className="twitter-timeline"
+                        data-width="700" 
+                        data-height="700"
+                        data-theme="light"
+                        href="https://twitter.com/Access_Live_?ref_src=twsrc%5Etfw"
+                        >
+                        Tweets by Access_Live
+                    </a>
                 </div>
-                <img src={DetailsChart} />
+                <div className="Details__flex_container--item-2">
+                    <img
+                    src={DetailsChart}
+                    />
+                </div>
             </div>
         </div>
     );
